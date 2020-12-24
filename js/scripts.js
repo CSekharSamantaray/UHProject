940f1115941532c7452aa042b4a881a26ed7d49d
CSekharSamantaray <60929176+CSekharSamantaray@users.noreply.github.com>
2020-12-24 14:15:20 +0530

    Modifications for UI Changes.
    Modifications for UI Changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-{
-    //Login Page Code inside this block.
+{  //Login Page Code inside this block.
 
-    function loginClick(){
+    function loginClick(){  //Login button click function call.
         var usr = document.getElementById("username").value;
         var pass = document.getElementById("password").value;
 
@@ -10,20 +9,45 @@
         warning.style.display = "none"; 
         // If username and password are vallid then navigate to the Home page.
         if (usr == "admin" && pass == "admin" ){ 
+            sessionStorage.setItem("username", usr);
             window.location.href = "default.html";
         }
         // If username and password are not vallidated then display the error message.
         else{ 
             warning.style.display = "block";
+            if(usr == ""){
+                warning.innerText = "Please enter the username.";
+            }
+            else if(pass == ""){
+                warning.innerText = "Please enter the password.";
+            }
+            else if (usr != "admin" || pass != "admin"){
+                warning.innerText = "The user credentials entered are incorrect. Please enter correct credentials to login.";
+            }
+            
         }
     }
 }
 
-{
-    //Home page Code inside this block. (default.html)
+{ //Home page Code inside this block. (default.html)
 
-    //Navbar menu option clicked function which accepts the menu name to process.
-    function menuClicked(menuName){
+    function bodyOnLoad(){  //bodyonload function call.
+        var username = sessionStorage.getItem("username");
+        if (username == null || username == ""){
+            window.location.href = "login.html";
+            return;
+        }
+        var loggedinuser = document.getElementById("loggedinuser");
+        loggedinuser.innerHTML = "<strong>Logged In User : " + sessionStorage.getItem("username") + "</strong>";
+    }
+
+    function logoutClicked(){  //Logout link function call.
+        sessionStorage.setItem("username", "");
+        window.location.href = "login.html";
+    }
+
+    
+    function menuClicked(menuName){  //Navbar menu option clicked function which accepts the menu name to process.
         if (menuName == 'Hotels'){
             $("#divPageContainer").load("hotels.html"); 
         }
@@ -48,5 +72,12 @@
         else if(menuName == 'Contact Us') {
             $("#divPageContainer").load("contactus.html"); 
         }
+    }
+}
+
+{  // Aviation Page Code inside this block. (aviation.html)
+
+    function searchFlight(){ //Search Flights button click function call.
+alert("Submit called");
     }
 }
